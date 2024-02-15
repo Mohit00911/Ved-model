8dc4c3b937f55f2ad7baed3c78dc67878806bf61
Mohit Rawat <mohitrawat@Mohits-MacBook-Air.local>
2024-02-15 17:29:28 +0530

    changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cors = require('cors');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const dataFilePath = path.join(__dirname, 'data', 'students.json');
 app.use(cors());
